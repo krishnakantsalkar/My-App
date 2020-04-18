131ffa7f194b3687312aad5e0348184e049dfc5c
krishnakantsalkar <krishnakantsalkar@gmail.com>
2020-04-19 00:04:10 +0530

    My-App: Finish Basic design
    Signed-off-by: krishnakantsalkar <krishnakantsalkar@gmail.com>

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  public email: string = 'Krishnakantsalkar@gmail.com';
+  public phone: string = '+919619705613';
+  public github: string = 'https://github.com/krishnakantsalkar';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

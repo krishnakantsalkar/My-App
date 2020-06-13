b55e0507365cca6c5ae2d4748c8e73aff99b5eac
krishnakantsalkar <krishnakantsalkar@gmail.com>
2020-06-13 23:32:08 +0530

    My-App: Lazy load About Page

---
+++ src/app/Components/about/about-routes.ts
@@ -0,0 +1,9 @@
+import { Route } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+
+export const aboutRoute: Route[] = [
+  {
+    path: '',
+    component: AboutComponent,
+  },
+];

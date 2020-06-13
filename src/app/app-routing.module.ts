af9a7db9721d327bf1c81fc56f6ba9c3c4175c4c
krishnakantsalkar <krishnakantsalkar@gmail.com>
2020-06-14 00:18:08 +0530

    My-App: Update Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainPageComponent } from './Components/main-page/main-page.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainPageComponent,
+  },
+  {
+    path: 'Home',
+    component: MainPageComponent,
+  },
+  {
+    path: 'About',
+    loadChildren: () =>
+      import('./Components/about/about.module').then((m) => m.AboutModule),
+  },
+  {
+    path: 'Blog',
+    loadChildren: () =>
+      import('./Components/blog/blog.module').then((m) => m.BlogModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

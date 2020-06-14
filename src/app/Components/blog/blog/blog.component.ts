0965daaf68b5a458b003ba8dcd52f79b5edc0f34
krishnakantsalkar <krishnakantsalkar@gmail.com>
2020-06-14 21:45:40 +0530

    My-App: Update Blog component

--- src/app/Components/blog/blog/blog.component.ts
+++ src/app/Components/blog/blog/blog.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.css']
+  styleUrls: ['./blog.component.css'],
 })
 export class BlogComponent implements OnInit {
+  public brightness: boolean;
+  public date = new Date();
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  mode() {
+    this.brightness = !this.brightness;
   }
-
 }

925303a498ffa526a28fb6325b2d2e2fec958799
krishnakantsalkar <krishnakantsalkar@gmail.com>
2020-06-14 21:44:19 +0530

    My-App: Mimic Backend data

---
+++ src/app/Components/data/data.ts
@@ -0,0 +1,141 @@
+export class blog {
+  public post1title: string = `Pixel Experience June Update for X00TD`;
+  public post1: string = ` The Android custom rom that I maintain for Asus Zenfone Max Pro M1 
+  has now reached June update ....
+  This time around I figured I'd add a somewhat controversial feature which was apparantly the meme-theme of this month.
+  'Tik-Tok' blocker...
+  While so many users loved the implementation (credits to its creator!) there was a negative reaction
+  by many of the users, which was kind of the point!, continuing on the shennanigans...
+  new kernel upstream for msm 4.4 completely derped the DTS eagle implementation. Build was still good to go!
+  Here's the Official post! 
+
+
+  🔔 Pixel Experience & Pixel Experience Plus Edition Android 10 Official update is up for X00T! 
+
+  📅 Build Date: 06/06/20 
+  
+  😎 By: @alex_mercer09 
+  
+  📝  Source Changelog:-
+  
+  -  June security patch
+  - Fixed recorder crashes
+  - Improved network traffic indicator
+  - Improved volume panel on left side (for supported devices)
+  - Updated translations
+  - Fix vibration enabled when battery saver is enabled 
+  - Implement quick QS brightness slider
+  - Updated some prebuilt system apps
+  - Other minor changes
+  
+  📲 Device Changelog:
+  
+  - added TikTok and other similar apps blocker
+  - Device tree improvements
+  
+  ✅ Thnx to all testers for help with bug fixing and evrything!
+  
+  ✏️ Notes:
+  
+  min firmware 059
+  magisk 20.3 recommended to pass CTS
+  incremental and OTA update available!
+  
+  #rom #Android10 #pe
+  Follow:@Asus_X00TD
+  Join:@AsusX00TD
+
+
+  `;
+  public postlink1: string = ` https://downloads.pixelexperience.org/X00TD`;
+
+  ///////
+
+  public post2title: string = `Counter strike: Global Offensive vs Valorant`;
+  public post2: string = ` 
+  
+  This new CS:GO-esque futuristic games seems to be taking the
+  spotlight off the famous game ...
+  
+  
+  Counterstrike Global Offensive launched in 2012 gained instant popularity..............
+  Being the HD version of the legendary Counter Strike series.
+
+  Valorant seems to broaden the concept of CS:GO by adding a futuristic theme and setting
+  along with keeping the origin concept of CS:GO intact
+  
+  New skins, character styles , fighting mechanism we get a an amazing experience!
+  The Game is entirely Free to play with in-game purchases 
+  
+  I'm Downloading it as this blog post goes live! 
+  
+
+  `;
+  public postlink2: string = null;
+  /////
+
+  public post3title: string = `What is Python ?`;
+  public post3: string = `
+  
+  What is Python? Executive Summary
+  
+  Python is an interpreted, object-oriented, high-level programming language with dynamic semantics.
+  Its high-level built in data structures, combined with dynamic typing and dynamic binding, make it very attractive for Rapid Application Development,
+  as well as for use as a scripting or glue language to connect existing components together. 
+  Python's simple, easy to learn syntax emphasizes readability and therefore reduces the cost of program maintenance. 
+  Python supports modules and packages, which encourages program modularity and code reuse.
+  
+  The Python interpreter and the extensive standard library are available in source or binary form without charge for all major platforms, 
+  and can be freely distributed.
+
+  Often, programmers fall in love with Python because of the increased productivity it provides.
+  Since there is no compilation step, the edit-test-debug cycle is incredibly fast.
+  Debugging Python programs is easy: a bug or bad input will never cause a segmentation fault. 
+  Instead, when the interpreter discovers an error, it raises an exception. When the program doesn't catch the exception, 
+  the interpreter prints a stack trace. A source level debugger allows inspection of local and global variables,
+  evaluation of arbitrary expressions, setting breakpoints, stepping through the code a line at a time, and so on. 
+  The debugger is written in Python itself, testifying to Python's introspective power.
+  On the other hand, often the quickest way to debug a program is to add a few print statements
+  to the source: the fast edit-test-debug cycle makes this simple approach very effective.
+  
+  
+  `;
+  public postlink3: string = null;
+  ////
+
+  public post4title: string = `Coronavirus continues its speedy climb...`;
+  public post4: string = ` 
+  
+  
+  As the Coronavirus cases continue to climb in India, 
+  Lockdown is lifted and we the citizens are left to fend for themselves against the deadly disease. 
+
+  The nature of citizens of India doesnt allow anything to stop them unless forced upon. 
+  No one seems to follow the prevention guidelines. Government may benefit from the medical costs of the virust toll ,
+  But thats just what I think. what you think? 
+  
+  Indians can now dine out, shop in malls and pray at religious sites for the first time in nearly 2 1/2 months. 
+  But masks or face coverings are mandatory and visitors will be thermally screened at entrances.
+  The easing of restrictions — the most significant since India went under lockdown in March — comes as 
+  the country records its biggest spike so far in new coronavirus cases in a single day.
+  India counted 9,983 new infections Monday, with its overall tally crossing 250,000. The death toll stands at 7,200.
+  Over the weekend, India surpassed Spain in the number of infections and became the fifth-worst affected country in the world.
+  Guided by little boxes painted on floors to maintain social distancing, worshippers offered prayers at 
+  mosques, temples and other religious sites across India. 
+  
+  But the faithful are not allowed to touch any idols or holy books. Sprinkling of sacred water, a common practice in many religions including Hinduism and Christianity, is also banned. 
+  People going to mosques have to bring their own prayer mats.
+  One shopping mall in the capital New Delhi has installed a sanitization enclosure at the entrance, spraying 
+  disinfectant mist when someone walks in. While most public places are open in Delhi, hotels remain closed to be available in case they are needed to serve as temporary hospitals.
+  Even though malls are open, gaming arcades and movie theaters inside them are still closed. 
+  In some states, shoppers are barred from trying on clothes and guests checking in to hotels have to report 
+  their travel and medical history.
+
+  India's lockdown, the largest in the world, was imposed in late March when the country had just over 500 COVID-19 cases.
+  About 100 million Indians have lost their jobs since then, prompting the government to reopen the economy 
+  even though scientists say the country is still far from reaching its peak infection rate.
+  Schools and colleges remain shut nationwide.
+  
+  
+  `;
+}

57b2c5cd750c477921e87bf063bd252882ee3cc6
krishnakantsalkar <krishnakantsalkar@gmail.com>
2020-06-15 15:17:51 +0530

    My-App: Dynamically present data on Blog page

--- src/app/Components/blog/blog/blog.component.ts
+++ src/app/Components/blog/blog/blog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { blog } from 'src/app/Shared/data';
 
 @Component({
   selector: 'app-blog',
@@ -8,13 +9,37 @@ import { Component, OnInit } from '@angular/core';
 export class BlogComponent implements OnInit {
   public brightness: boolean;
   public date = new Date();
+  public data1;
+  public data2;
+  public data3;
+  public data4;
+
+  public title1;
+  public title2;
+  public title3;
+  public title4;
+
   constructor() {}
 
   ngOnInit(): void {
     this.mode();
+    this.datadisplay();
   }
 
   mode() {
     this.brightness = JSON.parse(localStorage.getItem('mode'));
+  }
+
+  datadisplay() {
+    let blogdata = new blog();
+    this.data1 = blogdata.post1.substring(1, 90) + '....';
+    this.data2 = blogdata.post2.substring(1, 90) + '....';
+    this.data3 = blogdata.post3.substring(1, 90) + '....';
+    this.data4 = blogdata.post4.substring(1, 90) + '....';
+
+    this.title1 = blogdata.post1title;
+    this.title2 = blogdata.post2title;
+    this.title3 = blogdata.post3title;
+    this.title4 = blogdata.post4title;
   }
 }

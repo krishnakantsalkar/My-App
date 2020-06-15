c3863835bfd5ae45f19f730e08e110f8972d63f8
krishnakantsalkar <krishnakantsalkar@gmail.com>
2020-06-15 13:05:41 +0530

    My-App: Update About Page as per current design

--- src/app/Components/about/about/about.component.ts
+++ src/app/Components/about/about/about.component.ts
@@ -10,11 +10,17 @@ export class AboutComponent implements OnInit {
   public email: string = 'Krishnakantsalkar@gmail.com';
   public phone: string = '+919619705613';
   public github: string = 'https://github.com/krishnakantsalkar';
+  public brightness: boolean;
   constructor() {}
 
   ngOnInit() {
     AOS.init({
       startEvent: 'scroll',
     });
+
+    this.mode();
+  }
+  mode() {
+    this.brightness = JSON.parse(localStorage.getItem('mode'));
   }
 }

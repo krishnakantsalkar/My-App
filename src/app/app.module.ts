550dd1968b95c4887a2b7eb2316fb2949bfc1811
krishnakantsalkar <krishnakantsalkar@gmail.com>
2020-06-26 00:10:01 +0530

    My-App: Update providers and services in main module
    Signed-off-by: krishnakantsalkar <krishnakantsalkar@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './Components/main-page/main-page.component';
+import { userloginservices } from './Shared/services/userloginservice';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, MainPageComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [userloginservices],
   bootstrap: [AppComponent],
 })
 export class AppModule {}

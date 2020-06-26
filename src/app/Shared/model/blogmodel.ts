25b61ffd5ffbfc1b6d4096fb3087facfd19503d2
krishnakantsalkar <krishnakantsalkar@gmail.com>
2020-06-26 13:31:10 +0530

    My-App: add service and model for blog page
    * now that we hav backend support , remove all unnecessary code from frontend

---
+++ src/app/Shared/model/blogmodel.ts
@@ -0,0 +1,6 @@
+export interface Iblog {
+  postNumber: number;
+  postTitle: string;
+  post: string;
+  postLink?: string;
+}
---
+++ src/app/Shared/services/blogservice.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Iblog } from '../model/blogmodel';
+
+@Injectable({ providedIn: 'root' })
+export class blogpostservice {
+  public header: HttpHeaders;
+  public blogAPI: string = 'https://mybackend-1911.herokuapp.com/api/blog/Blog';
+  constructor(private http: HttpClient, private router: Router) {
+    this.header = new HttpHeaders({ 'Content-Type': 'application/json' });
+  }
+
+  getBlogs(): Observable<Iblog> {
+    return this.http.get<Iblog>(this.blogAPI);
+  }
+}

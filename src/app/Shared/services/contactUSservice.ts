e3edcef277252bf6ea88aa032dfbab6acd393877
krishnakantsalkar <krishnakantsalkar@gmail.com>
2020-08-01 16:32:24 +0530

    My-App: add service and model for contact us function
    Signed-off-by: krishnakantsalkar <krishnakantsalkar@gmail.com>

---
+++ src/app/Shared/services/contactUSservice.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { IcontactUs } from '../model/contactUsmodel';
+
+@Injectable({ providedIn: 'root' })
+export class contactService {
+  public header: HttpHeaders;
+  public contactAPI: string =
+    'https://mybackend-1911.herokuapp.com/api/contact/contactUs';
+  constructor(private http: HttpClient) {
+    {
+      this.header = new HttpHeaders({ 'Content-Type': 'application/json' });
+    }
+  }
+  contact(data): Observable<IcontactUs> {
+    return this.http.post<IcontactUs>(this.contactAPI, JSON.stringify(data), {
+      headers: this.header,
+    });
+  }
+}

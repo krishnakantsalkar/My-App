8ca1552ec22396311a3176e169167234ec9df6b9
krishnakantsalkar <dev@krishnakantsalkar.gq>
2020-08-28 17:39:53 +0530

    My-App: Re-design About page
    *material design
    *no unnecessary animations
    *removed unnecessary image assets
    *updated new assets
    *added all updates from @9c3a5a5, @209ff5f etc.
    
    Signed-off-by: krishnakantsalkar <dev@krishnakantsalkar.gq>

--- src/app/Components/about/about/about.component.ts
+++ src/app/Components/about/about/about.component.ts
@@ -10,6 +10,7 @@ export class AboutComponent implements OnInit {
   public email: string = 'dev@krishnakantsalkar.gq';
   public phone: string = '+919619705613';
   public github: string = 'https://github.com/krishnakantsalkar';
+  public copied;
   public brightness: boolean;
   constructor() {}
 
@@ -19,8 +20,105 @@ export class AboutComponent implements OnInit {
     });
 
     this.mode();
+
+    //Get the button
+    var mybutton = document.getElementById('myBtn');
+    let mediaQ = window.matchMedia('(max-width: 600px)');
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function () {
+      scrollFunction();
+      scrollFunctionMedia(mediaQ);
+    };
+
+    function scrollFunction() {
+      if (
+        document.body.scrollTop > 50 ||
+        document.documentElement.scrollTop > 50
+      ) {
+        mybutton.style.display = 'block';
+        document.getElementById('top-panel').style.height = '75px';
+        document.getElementById('top-panel').style.transitionDuration = '0.3s';
+        document.getElementById('top-panel').style.transitionTimingFunction =
+          'ease-in';
+        document.getElementById('top-panel-nametag').style.paddingTop = '20px';
+        document.getElementById('links').style.marginTop = '5px';
+      } else {
+        mybutton.style.display = 'none';
+        document.getElementById('top-panel').style.height = '90px';
+        document.getElementById('top-panel-nametag').style.paddingTop = '25px';
+        document.getElementById('links').style.marginTop = '10px';
+      }
+    }
+
+    function scrollFunctionMedia(mediaQuery) {
+      if (
+        (mediaQuery.matches && document.body.scrollTop > 50) ||
+        (mediaQuery.matches && document.documentElement.scrollTop > 50)
+      ) {
+        document.getElementById('top-panel').style.height = '85px';
+        document.getElementById('top-panel-nametag').style.paddingTop = '15px';
+        document.getElementById('links').style.marginTop = '-15px';
+      } else if (
+        (mediaQuery.matches && document.body.scrollTop < 50) ||
+        (mediaQuery.matches && document.documentElement.scrollTop < 50)
+      ) {
+        document.getElementById('top-panel').style.height = '110px';
+        document.getElementById('top-panel-nametag').style.paddingTop = '25px';
+        document.getElementById('links').style.marginTop = '-5px';
+      }
+    }
+
+    textSequence(0);
+    async function textSequence(i) {
+      var textLoop = [
+        '"Indulge in the fast Experience.. Build the Future!"',
+        `"Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live.
+         - Martin Golding"`,
+        '"Computers make very fast, very accurate mistakes."',
+        '"Only half of programming is coding. The other 90% is debugging."',
+        '"There is no place like 127.0.0.1"',
+      ];
+
+      if (document.getElementById('loopQuotes')) {
+        if (textLoop.length > i) {
+          setTimeout(function () {
+            if (document.getElementById('loopQuotes')) {
+              document.getElementById('loopQuotes').innerHTML = textLoop[i];
+              document.getElementById('loopQuotes').style.fontFamily =
+                "'Lucida Console', 'LucidaConsole', 'monospace'";
+              textSequence(++i);
+            }
+          }, 3000); // enter seconds (in milliseconds)
+        } else if (textLoop.length == i) {
+          // Loop
+          textSequence(0);
+        }
+      }
+    }
   }
   mode() {
     this.brightness = JSON.parse(localStorage.getItem('mode'));
+  }
+
+  scrollTo(id) {
+    document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+  }
+
+  copyToClip() {
+    var data = document.createElement('input');
+    data.setAttribute('value', document.getElementById('copyText').innerHTML);
+    var elemnt = document.getElementById('copyText').innerHTML;
+    document.body.appendChild(data);
+    data.select();
+    document.execCommand('copy');
+    this.copied = elemnt;
+    setTimeout(() => {
+      document.getElementById('copyAlert').classList.add('hidden');
+    }, 3000);
+  }
+
+  topFunction() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   }
 }

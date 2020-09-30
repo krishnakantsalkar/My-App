23158b89363a774a62e791e0a8ff1de6638d8a9a
krishnakantsalkar <dev@krishnakantsalkar.gq>
2020-09-30 11:16:49 +0530

    My-App: clean up About Page
    Signed-off-by: krishnakantsalkar <dev@krishnakantsalkar.gq>

--- src/app/Components/about/about/about.component.ts
+++ src/app/Components/about/about/about.component.ts
@@ -114,55 +114,6 @@ export class AboutComponent implements OnInit {
     // brightness mode
     this.mode();
 
-    //Get the button
-    var mybutton = document.getElementById('myBtn');
-    let mediaQ = window.matchMedia('(max-width: 600px)');
-
-    // When the user scrolls down 20px from the top of the document, show the button
-    window.onscroll = function () {
-      scrollFunction();
-      scrollFunctionMedia(mediaQ);
-    };
-
-    // various methods on scroll
-    function scrollFunction() {
-      if (
-        document.body.scrollTop > 50 ||
-        document.documentElement.scrollTop > 50
-      ) {
-        mybutton.style.bottom = '20px';
-        document.getElementById('top-panel').style.height = '75px';
-        document.getElementById('top-panel').style.transitionDuration = '0.3s';
-        document.getElementById('top-panel').style.transitionTimingFunction =
-          'ease-in';
-        document.getElementById('top-panel-nametag').style.paddingTop = '20px';
-        document.getElementById('links').style.marginTop = '5px';
-      } else {
-        mybutton.style.bottom = '-50px';
-        document.getElementById('top-panel').style.height = '90px';
-        document.getElementById('top-panel-nametag').style.paddingTop = '25px';
-        document.getElementById('links').style.marginTop = '10px';
-      }
-    }
-
-    function scrollFunctionMedia(mediaQuery) {
-      if (
-        (mediaQuery.matches && document.body.scrollTop > 50) ||
-        (mediaQuery.matches && document.documentElement.scrollTop > 50)
-      ) {
-        document.getElementById('top-panel').style.height = '85px';
-        document.getElementById('top-panel-nametag').style.paddingTop = '15px';
-        document.getElementById('links').style.marginTop = '-15px';
-      } else if (
-        (mediaQuery.matches && document.body.scrollTop < 50) ||
-        (mediaQuery.matches && document.documentElement.scrollTop < 50)
-      ) {
-        document.getElementById('top-panel').style.height = '110px';
-        document.getElementById('top-panel-nametag').style.paddingTop = '25px';
-        document.getElementById('links').style.marginTop = '-5px';
-      }
-    }
-
     // text loop
     textSequence(0);
     async function textSequence(i) {

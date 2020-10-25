e9a496633df15592c08a50ebb9616e5d56113e63
krishnakantsalkar <dev@krishnakantsalkar.gq>
2020-10-25 13:37:30 +0530

    My-App: switch to different covid-19 india tracker API
    * credits: http://covidindiatracker.com/
    
    Signed-off-by: krishnakantsalkar <dev@krishnakantsalkar.gq>

--- src/app/Shared/services/covidTrackerApi.ts
+++ src/app/Shared/services/covidTrackerApi.ts
@@ -4,12 +4,19 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Injectable({ providedIn: 'root' })
 export class covidApiService {
   public header: HttpHeaders;
-  public covidApi: string = 'https://api.apify.com/v2/key-value-stores/toDWvRj1JpTXiM8FF/records/LATEST?disableRedirect=true';
+  public covidApiState: string = 'https://api.covidindiatracker.com/state_data.json'
+  public covidApiTotal:string = 'https://api.covidindiatracker.com/total.json'
   constructor(private http: HttpClient) {
     this.header = new HttpHeaders({ 'Content-Type': 'application/json' });
   }
 
-  getcovidData(){
-return this.http.get(this.covidApi)
+// district wise data
+getCovidData2(){
+  return this.http.get(this.covidApiState)
+}
+
+// total data
+getCovidDataTotal(){
+  return this.http.get(this.covidApiTotal)
 }
 }

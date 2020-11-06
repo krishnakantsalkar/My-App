a0e761519ba0a88146b9193428d0a0654c64f32d
krishnakantsalkar <dev@krishnakantsalkar.gq>
2020-11-06 12:39:47 +0530

    My-App: add common footer to 'Legal' section
    Signed-off-by: krishnakantsalkar <dev@krishnakantsalkar.gq>

--- src/app/Components/disclaimer/disclaimer.module.ts
+++ src/app/Components/disclaimer/disclaimer.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { DisclaimerComponent } from './disclaimer/disclaimer.component';
 import { RouterModule } from '@angular/router';
 import { disclaimerRoutes } from './disclaimer-routes';
+import { SharedModuleModule } from '../shared-module/shared-module.module';
 
 @NgModule({
   declarations: [DisclaimerComponent],
-  imports: [CommonModule, RouterModule.forChild(disclaimerRoutes)],
+  imports: [CommonModule, RouterModule.forChild(disclaimerRoutes),SharedModuleModule],
 })
 export class DisclaimerModule {}
--- src/app/Components/privacy/privacy.module.ts
+++ src/app/Components/privacy/privacy.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { PrivacyComponent } from './privacy/privacy.component';
 import { RouterModule } from '@angular/router';
 import { privacyRoutes } from './privacy-routes';
+import { SharedModuleModule } from '../shared-module/shared-module.module';
 
 @NgModule({
   declarations: [PrivacyComponent],
-  imports: [CommonModule, RouterModule.forChild(privacyRoutes)],
+  imports: [CommonModule, RouterModule.forChild(privacyRoutes), SharedModuleModule],
 })
 export class PrivacyModule {}

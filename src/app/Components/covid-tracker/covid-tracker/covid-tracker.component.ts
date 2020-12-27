77346add5aa8ec5212faefe15bb3b38f12bdd26f
krishnakantsalkar <dev@krishnakantsalkar.gq>
2020-12-27 17:32:05 +0530

    My-App: track total views on covid datatable
    Signed-off-by: krishnakantsalkar <dev@krishnakantsalkar.gq>

--- src/app/Components/covid-tracker/covid-tracker/covid-tracker.component.ts
+++ src/app/Components/covid-tracker/covid-tracker/covid-tracker.component.ts
@@ -4,157 +4,179 @@ import { Title } from '@angular/platform-browser';
 import * as AOS from 'aos';
 import { modeService } from '../../../Shared/services/light-dark-Modeservice';
 import { userloginservices } from '../../../Shared/services/userloginservice';
+import { clientIpService } from '../../../Shared/services/clientip-service';
+import { userIp } from '../../../Shared/model/userViewModel';
 
 @Component({
   selector: 'app-covid-tracker',
   templateUrl: './covid-tracker.component.html',
-  styleUrls: ['./covid-tracker.component.css']
+  styleUrls: ['./covid-tracker.component.css'],
 })
 export class CovidTrackerComponent implements OnInit {
-public brightness
-public covidData
-public updateTime
-public totalCovidData
+  public brightness;
+  public covidData;
+  public updateTime;
+  public totalCovidData;
 
-public pageTitle = 'Covid-19 Tracker'
+  public pageTitle = 'Covid-19 Tracker';
 
-public pageUrl
- 
-public dtOptions: DataTables.Settings = {}
+  public pageUrl;
 
-public adminCheck
+  public dtOptions: DataTables.Settings = {};
 
-public today
-public yesterday
-public dayBefore
-public month
-public monthNames:string[] = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
-];
+  public adminCheck;
 
-  constructor(private covidApi: covidApiService, private titleService:Title, private defaultModeService: modeService, private loginService: userloginservices) {}
+  public today;
+  public yesterday;
+  public dayBefore;
+  public month;
+  public monthNames: string[] = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  public totalViews;
+
+  constructor(
+    private covidApi: covidApiService,
+    private titleService: Title,
+    private defaultModeService: modeService,
+    private loginService: userloginservices,
+    private clientservice: clientIpService
+  ) {}
 
   ngOnInit(): void {
-
     // method calls
 
     // brightness mode
-    this.defaultModeService.modeSwitch.subscribe(item=>{
-      this.brightness = item
-    })
+    this.defaultModeService.modeSwitch.subscribe((item) => {
+      this.brightness = item;
+    });
 
-   // aos animations
-   AOS.init({
-    startEvent: 'DomContentLoaded',
-  });
+    // aos animations
+    AOS.init({
+      startEvent: 'DomContentLoaded',
+    });
 
-     //set page title  
-     this.titleService.setTitle(this.pageTitle)
+    //set page title
+    this.titleService.setTitle(this.pageTitle);
 
     //  load today's covid data
-     this.getTodayCovidData()
+    this.getTodayCovidData();
 
     //  covid total stats
-    this.covidApi.getCovidDataTotal().subscribe(item=>{ 
-      this.totalCovidData = item
-          // console.log(this.totalCovidData)    
-    }) 
+    this.covidApi.getCovidDataTotal().subscribe((item) => {
+      this.totalCovidData = item;
+      // console.log(this.totalCovidData)
+    });
 
     // check admin presence
-    this.loginService.currentUsers.subscribe(item => {
-      this.adminCheck = item
-    })
+    this.loginService.currentUsers.subscribe((item) => {
+      this.adminCheck = item;
+    });
 
     //get page url
-    this.pageUrl = window.location.href
-   
+    this.pageUrl = window.location.href;
 
-    //current time 
-    this.currentTime()
-    
+    //current time
+    this.currentTime();
+
     // options for dataTables
     this.dtOptions = {
       pagingType: 'full_numbers',
       pageLength: 19,
       ordering: true,
-      order: [1,'desc'],
+      order: [1, 'desc'],
       searching: true,
       search: {
-        caseInsensitive: true
+        caseInsensitive: true,
       },
       responsive: true,
-      stateSave: true
+      stateSave: true,
     };
 
-
     // initialize dates
-    this.today = new Date().getDate()
-    this.yesterday = this.today - 1
-    this.dayBefore = this.today - 2
+    this.today = new Date().getDate();
+    this.yesterday = this.today - 1;
+    this.dayBefore = this.today - 2;
     // initialize month
-    let currentDate = new Date()
-    this.month = this.monthNames[currentDate.getMonth()]
- 
+    let currentDate = new Date();
+    this.month = this.monthNames[currentDate.getMonth()];
 
     // show/hide history options
     $(document).ready(() => {
-      $('.historyOptions').hide()
-
-    })
+      $('.historyOptions').hide();
+    });
     $('.showHistory').on('click', () => {
-      $('.historyOptions').show(300)
-    })
+      $('.historyOptions').show(300);
+    });
 
     $('.hideHistory').on('click', () => {
-      $('.historyOptions').hide(300)
-    })
+      $('.historyOptions').hide(300);
+    });
+
+    this.clientservice.getClientIp().subscribe((item: userIp) => {
+      let data = item.ip;
+
+      this.covidApi.trackViews(data).subscribe((item2) => {
+        this.totalViews = item2.result;
+      });
+    });
   }
 
-// current time
-  currentTime(){
-
-  let checkUpdatedTime = sessionStorage.getItem('covidData')
-  if(!checkUpdatedTime){
-
-  let d = new Date()
-  this.updateTime = d.toLocaleString()
-  sessionStorage.setItem('covidData', this.updateTime)
-
-  } else {
-  this.updateTime = sessionStorage.getItem('covidData')
+  // current time
+  currentTime() {
+    let checkUpdatedTime = sessionStorage.getItem('covidData');
+    if (!checkUpdatedTime) {
+      let d = new Date();
+      this.updateTime = d.toLocaleString();
+      sessionStorage.setItem('covidData', this.updateTime);
+    } else {
+      this.updateTime = sessionStorage.getItem('covidData');
+    }
   }
-}
 
-  // get latest covid Data 
-  getTodayCovidData(){
-  this.covidApi.getCovidData2().subscribe(item=>{
-    this.covidData = null
-    this.covidData = item
-    // console.log(this.covidData)
-  })
-}
-
+  // get latest covid Data
+  getTodayCovidData() {
+    this.covidApi.getCovidData2().subscribe((item) => {
+      this.covidData = null;
+      this.covidData = item;
+      // console.log(this.covidData)
+    });
+  }
 
   // store data to db
-  sendData(){
+  sendData() {
+    if (confirm('Send data to database?')) {
+      this.covidApi.storeCovidData(this.covidData).subscribe(
+        (item) => {
+          console.log(item);
+          alert('Data stored to DB');
+        },
+        (err) => {
+          alert('Something went wrong! check console');
+        }
+      );
+    } else {
+      return;
+    }
+  }
 
-  if(confirm('Send data to database?')){
-    this.covidApi.storeCovidData(this.covidData).subscribe(item=>{
-    console.log(item)
-    alert('Data stored to DB')
-    }, err => {
-    alert('Something went wrong! check console')
-    })
-   } else {
-     return
-   }
- }
-  
   // get data by date
-  getData(date){
-    this.covidApi.getStoredCovidData(date).subscribe(item=>{
-      this.covidData = null 
-      this.covidData = item[0].data
-    })
-  } 
+  getData(date) {
+    this.covidApi.getStoredCovidData(date).subscribe((item) => {
+      this.covidData = null;
+      this.covidData = item[0].data;
+    });
+  }
 }
--- src/app/Shared/services/covidTrackerApi.ts
+++ src/app/Shared/services/covidTrackerApi.ts
@@ -4,36 +4,46 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Injectable({ providedIn: 'root' })
 export class covidApiService {
   public header: HttpHeaders;
-  public covidApiState: string = 'https://api.covidindiatracker.com/state_data.json'
-  public covidApiTotal:string = 'https://api.covidindiatracker.com/total.json'
+  public covidApiState: string =
+    'https://api.covidindiatracker.com/state_data.json';
+  public covidApiTotal: string = 'https://api.covidindiatracker.com/total.json';
 
   // stored covid data
-  public covidDataStore: string = 'https://mybackend-1911.herokuapp.com/api/covid/storeCovidData/';
-  public getCovidDataStore: string = 'https://mybackend-1911.herokuapp.com/api/covid/getOldCovidData/';
+  public covidDataStore: string =
+    'https://mybackend-1911.herokuapp.com/api/covid/storeCovidData/';
+  public getCovidDataStore: string =
+    'https://mybackend-1911.herokuapp.com/api/covid/getOldCovidData/';
+
+  public covidViewStore: string =
+    'https://mybackend-1911.herokuapp.com/api/covid/covidDataView';
 
   constructor(private http: HttpClient) {
-    this.header = new HttpHeaders({ 'Content-Type': 'application/json',
-    'x-auth-token': JSON.parse(localStorage.getItem('userToken')) 
+    this.header = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'x-auth-token': JSON.parse(localStorage.getItem('userToken')),
     });
   }
 
-// district wise data
-getCovidData2(){
-  return this.http.get(this.covidApiState)
-}
+  // district wise data
+  getCovidData2() {
+    return this.http.get(this.covidApiState);
+  }
 
-// total data
-getCovidDataTotal(){
-  return this.http.get(this.covidApiTotal)
-}
+  // total data
+  getCovidDataTotal() {
+    return this.http.get(this.covidApiTotal);
+  }
 
-// stored covid data methods
-storeCovidData(data){
-  return this.http.post(this.covidDataStore, data, {headers:this.header})
-}
+  // stored covid data methods
+  storeCovidData(data) {
+    return this.http.post(this.covidDataStore, data, { headers: this.header });
+  }
 
-getStoredCovidData(date){
-  return this.http.get<any>(this.getCovidDataStore + date)
-}
+  getStoredCovidData(date) {
+    return this.http.get<any>(this.getCovidDataStore + date);
+  }
 
+  trackViews(data) {
+    return this.http.put<any>(this.covidViewStore, data);
+  }
 }

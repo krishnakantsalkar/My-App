df27d4bca69c63d0dbeb8352e3c34f65eb50d14a
krishnakantsalkar <dev@krishnakantsalkar.in>
2021-12-25 23:20:55 +0530

    app: add & setup Angular Material
    Signed-off-by: krishnakantsalkar <dev@krishnakantsalkar.in>

--- src/app/Components/shared-module/shared-module.module.ts
+++ src/app/Components/shared-module/shared-module.module.ts
@@ -2,18 +2,79 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { ToastModule } from 'primeng/toast';
-import { TooltipModule } from 'primeng/tooltip';
-import { DialogModule } from 'primeng/dialog';
-import { ButtonModule } from 'primeng/button';
-import { ConfirmDialogModule } from 'primeng/confirmdialog';
+
 import { SkeletonModule } from 'primeng/skeleton';
 import { QuillModule } from 'ngx-quill';
-import { CardModule } from 'primeng/card';
-import { TabViewModule } from 'primeng/tabview';
-import { CheckboxModule } from 'primeng/checkbox';
-import { RadioButtonModule } from 'primeng/radiobutton';
+
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule, MatChipList } from '@angular/material/chips';
+import { MatStepperModule } from '@angular/material/stepper';
+import {
+  MatDatepickerModule,
+  MatDatepicker,
+} from '@angular/material/datepicker';
+import { MatDialogActions, MatDialogModule } from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRippleModule, MatNativeDateModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatRadioModule } from '@angular/material/radio';
 // Shared module
+
+const MaterialComponents = [
+  MatButtonModule,
+  MatBadgeModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatTabsModule,
+  MatTooltipModule,
+  MatDividerModule,
+  MatNativeDateModule,
+  MatRadioModule,
+];
 
 @NgModule({
   declarations: [],
@@ -21,30 +82,16 @@ import { RadioButtonModule } from 'primeng/radiobutton';
     CommonModule,
     ReactiveFormsModule,
     RouterModule,
-    ToastModule,
-    TooltipModule,
-    DialogModule,
-    ButtonModule,
-    ConfirmDialogModule,
     SkeletonModule,
     QuillModule,
-    CardModule,
-    TabViewModule,
-    CheckboxModule,
-    RadioButtonModule,
+    MaterialComponents,
   ],
-  exports: [
-    ToastModule,
-    TooltipModule,
-    DialogModule,
-    ButtonModule,
-    ConfirmDialogModule,
-    SkeletonModule,
-    QuillModule,
-    CardModule,
-    TabViewModule,
-    CheckboxModule,
-    RadioButtonModule,
+  exports: [SkeletonModule, QuillModule, MaterialComponents],
+  providers: [
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: { appearance: 'fill' },
+    },
   ],
 })
 export class SharedModuleModule {}

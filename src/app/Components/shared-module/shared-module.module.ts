d86e88e35139e8fbc5669fdea184f6a56eb74db9
krishnakantsalkar <dev@krishnakantsalkar.in>
2022-12-03 11:51:51 +0530

    app: prep for Angular Material v15
    Signed-off-by: krishnakantsalkar <dev@krishnakantsalkar.in>

--- src/app/Components/shared-module/shared-module.module.ts
+++ src/app/Components/shared-module/shared-module.module.ts
@@ -13,7 +13,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatChipsModule, MatChipList } from '@angular/material/chips';
+import { MatChipsModule } from '@angular/material/chips';
 import { MatStepperModule } from '@angular/material/stepper';
 import {
   MatDatepickerModule,
